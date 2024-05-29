3fbc878b7b9e3bf819624f3b6f066b9dfe710fa0
Fabio Junior <fabiopirespjunior@gmail.com>
2024-05-29 16:22:56 -0300

    commits dia 29/05/24

---
+++ src/Entities/Shape.java
@@ -0,0 +1,21 @@
+package Entities;
+
+public abstract class Shape {
+
+    private Color color;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Shape() {
+    }
+    public Shape(Color color){
+        this.color = color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+    public abstract double area();
+}
